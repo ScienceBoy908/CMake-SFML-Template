b18428d8246559929a1f993e2eaf72cc9a9287d8
ScienceBoy908 <95256109+ScienceBoy908@users.noreply.github.com>
2024-10-11 14:21:11 -0400

    Delete src directory
    Being replaced for a more organized layout.

--- src/config.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace conf
-{
-	inline unsigned int fps = 144;
-	inline sf::Vector2u windowSizeU = { 1920u, 1080u };
-	inline sf::Vector2f windowSizeF = static_cast<sf::Vector2f>(windowSizeU);
-}
--- src/resource_handler.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include "embedded_resources.hpp"
-#include <stdexcept>
-
-void debug_resources();
-std::vector<unsigned char> get_resource_data(std::string path);
