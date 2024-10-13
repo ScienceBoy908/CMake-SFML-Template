dcc0115e453f646f80c32d8a747b9f129177d0d1
ScienceBoy908 <95256109+ScienceBoy908@users.noreply.github.com>
2024-10-13 14:48:24 -0400

    Update sfMath.hpp
    Added some vector math

--- src/Engine/SFMath/sfMath.hpp
+++ src/Engine/SFMath/sfMath.hpp
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <algorithm>
 #include <iostream>
+#include <cmath>
 
 namespace sfMath
 {
@@ -19,4 +20,15 @@ namespace sfMath
 	sf::Color LerpColor(const sf::Color& a, const sf::Color& b, float t);
 
 	void CenterAlignText(sf::Text& txt);
+
+	float GetVectorAngle(const sf::Vector2f& vector);
+	float GetVectorLength(const sf::Vector2f& vector);
+
+	sf::Vector2f RotateVector(const sf::Vector2f& vector, float angleRadians);
+
+	sf::Vector2f NormalizeVector(const sf::Vector2f& vector, float length);
+
+	float Rad(float deg);
+	float Deg(float rad);
+
 }
