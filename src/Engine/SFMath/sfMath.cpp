6f265e83ac796aaaaed7c5fd023026329d2ae334
ScienceBoy908 <95256109+ScienceBoy908@users.noreply.github.com>
2024-10-13 14:48:53 -0400

    Update sfMath.cpp
    Added vector math.

--- src/Engine/SFMath/sfMath.cpp
+++ src/Engine/SFMath/sfMath.cpp
@@ -19,9 +19,50 @@ namespace sfMath {
             Lerp<sf::Uint8>(a.a, b.a, t)
         );
     }
+
     void CenterAlignText(sf::Text& txt)
     {
         sf::FloatRect txtBounds = txt.getLocalBounds();
         txt.setOrigin(txtBounds.width / 2, txtBounds.height / 2);
+    }
+
+    float GetVectorAngle(const sf::Vector2f& vector)
+    {
+        return std::atan2(vector.y, vector.x);
+    }
+    float GetVectorLength(const sf::Vector2f& vector) {
+        return std::sqrt(vector.x * vector.x + vector.y * vector.y);
+    }
+
+    sf::Vector2f RotateVector(const sf::Vector2f& vector, float angleRadians)
+    {
+        float cosTheta = std::cos(angleRadians);
+        float sinTheta = std::sin(angleRadians);
+
+        // Apply the rotation transformation
+        return sf::Vector2f(
+            vector.x * cosTheta - vector.y * sinTheta,
+            vector.x * sinTheta + vector.y * cosTheta
+        );
+    }
+
+    sf::Vector2f NormalizeVector(const sf::Vector2f& vector, float toLength=1)
+    {
+        float length = GetVectorLength(vector) * toLength;
+        if (length != 0) {
+            return sf::Vector2f(vector.x / length, vector.y / length);
+        }
+        else {
+            return sf::Vector2f(0.f, toLength);  // Return zero vector if length is zero
+        }
+    }
+
+    float Rad(float deg)
+    {
+        return deg *  3.14159265f / 180.f;
+    }
+    float Deg(float rad) 
+    {
+        return rad * 180.f / 3.14159265f;
     }
 }
